5c2093da0d1f9d0762de4dfeddaf8ec0f3998d9d
MrArturtleNSCDS <31484951+MrArturtleNSCDS@users.noreply.github.com>
2020-03-09 11:26:17 -0500

    Create boxes.js

---
+++ boxes.js
@@ -0,0 +1,32 @@
+$('.color1, .color2').click(
+    function(){
+        var myColor = $(this).css('background-color');
+        $('#display').css('background-color', myColor);
+    }
+)
+
+$('.color3').click(
+    function(){
+        var myColor = $(this).css('background-color');
+        $('#display').css('background-color', myColor);
+        $('#display').css('color', $(this).css('color'));
+        $('#display').text($(this).text());
+    }
+);
+
+$('#box5').click(
+    function(){
+        var myColor = $(this).css('background-color');
+        $('#display').css('background-color', myColor);
+        var randNum = Math.floor(Math.random()*20)+10;
+        $('#box6').text(randNum);
+    }
+);
+
+$('#box6').click(
+    function(){
+        var myColor = $(this).css('background-color');
+        $('#display').css('background-color', myColor);
+        $('#display').css('color', $(this).css('color'));
+    }
+);
