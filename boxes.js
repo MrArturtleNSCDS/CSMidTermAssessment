f5efa87db59ba9ae9eba6a8e97b550614e923f98
MrArturtleNSCDS <agarcia@nscds.org>
2020-03-09 17:25:16 +0000

    Assessment Done

--- boxes.js
+++ boxes.js
@@ -20,6 +20,7 @@ $('#box5').click(
         $('#display').css('background-color', myColor);
         var randNum = Math.floor(Math.random()*20)+10;
         $('#box6').text(randNum);
+        $('#display').text($(this).text());
     }
 );
 
@@ -28,5 +29,18 @@ $('#box6').click(
         var myColor = $(this).css('background-color');
         $('#display').css('background-color', myColor);
         $('#display').css('color', $(this).css('color'));
+    }
+);
+
+
+for(var r=0; r<12; r++){
+    for(var c=0; c<12; c++){
+        $('#wrapperBox').append("<div class='pixel'></div>");
+    }
+}
+
+$('.pixel').click(
+    function(){
+        $(this).css('background-color', 'darkgreen');
     }
 );
